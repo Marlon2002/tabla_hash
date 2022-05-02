96cf20c21dc4fad7ed51f1eeb91abf96a49d1b45
Marlon2002 <mbarriosm15@miumg.edu.gt>
2022-05-01 21:14:58 -0600

    table-hash

---
+++ src/tablas_hash/Table_hash_main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablas_hash;
+import Vista.*;
+/**
+ *
+ * @author joseg
+ */
+public class Table_hash_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Hash_form Hash = new Hash_form();
+        Hash.show();
+        
+    }
+    
+}
